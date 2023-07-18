8c100d5fcfca7d57ffb99dddbd7c8b663b93e28f
yangc <limyangchae@kakao.com>
2023-07-18 09:56:06 +0900

    20230718 작업반영

--- src/main/java/tobyspring/config/MyAutoConfigImportSelector.java
+++ src/main/java/tobyspring/config/MyAutoConfigImportSelector.java
@@ -1,14 +1,38 @@
 package tobyspring.config;
 
+import org.springframework.boot.context.annotation.ImportCandidates;
 import org.springframework.context.annotation.DeferredImportSelector;
 import org.springframework.core.type.AnnotationMetadata;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.StreamSupport;
+
 public class MyAutoConfigImportSelector implements DeferredImportSelector {
+
+    private final ClassLoader classLoader;
+
+    public MyAutoConfigImportSelector(ClassLoader classLoader) {
+        this.classLoader = classLoader;
+    }
+
+
     @Override
     public String[] selectImports(AnnotationMetadata importingClassMetadata) {
-        return new String[] {
-                "tobyspring.config.autoconfig.DispatcherServletConfig",
-                "tobyspring.config.autoconfig.TomcatWebServerConfig"
-        };
+
+//        Iterable<String> candidates =  ImportCandidates.load(MyAutoConfiguration.class, classLoader);
+//        return StreamSupport.stream(candidates.spliterator(), false).toArray(String[]::new);
+
+        List<String> autoConfigs = new ArrayList<String>();
+        Iterable<String> candidates =  ImportCandidates.load(MyAutoConfiguration.class, classLoader);
+        for(String candidate : candidates) {
+            autoConfigs.add(candidate);
+        }
+        return autoConfigs.toArray(new String[0]);
+
+//        return new String[] {
+//                "tobyspring.config.autoconfig.DispatcherServletConfig",
+//                "tobyspring.config.autoconfig.TomcatWebServerConfig"
+//        };
     }
 }
